f9eefe40abd0bfa5e900b6c6533124427aaf7ef8
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2015-05-31 21:13:15 +0100

    implemented PUT, PATCH & DELETE

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -30,13 +30,64 @@ var routes = function (Product) {
             });
         });
 
+    webRouter.use('/:productId', function (req, res, next) {
+        Product.findById(req.params.productId, function (err, data) {
+            if (err) {
+                res.status(500).send(err);
+            } else if (data) {
+                req.product = data;
+                next();
+            } else {
+                res.status(404).send('Product not found');
+            }
+        });
+    });
+
     webRouter.route('/:productId')
         .get(function (req, res) {
-            Product.findById(req.params.productId, function (err, data) {
+            res.send(req.product);
+        })
+        .put(function (req, res) {
+            req.product.productName = req.body.productName;
+            req.product.productCode = req.body.productCode;
+            req.product.description = req.body.description;
+            req.product.releaseDate = req.body.releaseDate;
+            req.product.category = req.body.category;
+            req.product.cost = req.body.cost;
+            req.product.price = req.body.price;
+            req.product.imageUrl = req.body.imageUrl;
+            req.product.tags = req.body.tags;
+            req.product.active = req.body.active;
+            req.product.save(function (err) {
                 if (err) {
                     res.status(500).send(err);
                 } else {
-                    res.json(data);
+                    res.json(req.product);
+                }
+            });
+
+        })
+        .patch(function (req, res) {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+            for (var p in req.body) {
+                req.product[p] = req.body[p];
+            }
+            req.product.save(function (err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.product);
+                }
+            });
+        })
+        .delete(function (req, res) {
+            req.product.remove(function (err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send('Product removed');
                 }
             });
         });
